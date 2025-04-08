dad99857d334a32c852a27ff7bcf34b499bcb83d
Mouctar <Mr.conte@icloud.com>
2025-04-08 14:48:11 +0200

    Responsive

--- src/components/section/home/Hero.jsx
+++ src/components/section/home/Hero.jsx
@@ -4,12 +4,14 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col justify-center items-center h-screen">
-      <div className="text-center space-y-6">
-        <h1 className={`${dmSerif.className} text-5xl`}>
+    <section className="h-screen py-24 px-6 sm:px-12 lg:px-24 ">
+      <div className="max-w-5xl mx-auto text-center space-y-6">
+        <h1
+          className={`${dmSerif.className} text-4xl sm:text-5xl lg:text-6xl leading-tight`}
+        >
           Un site web à la hauteur de vos <AuroraText> ambitions</AuroraText>
         </h1>
-        <p className="text-lg">
+        <p className=" text-lg sm:text-xl leading-relaxed">
           Je conçois des sites professionnels sur-mesure <br /> rapides,
           élégants et optimisés <br /> pour renforcer l’image de votre
           entreprise et attirer de nouveaux clients.
