2e0f3a10f707552903df65cec8452601a06043ca
Mouctar <Mr.conte@icloud.com>
2025-06-09 17:42:58 +0200

    Analytics

---
+++ src/components/layout/GoogleAnalytics.jsx
@@ -0,0 +1,20 @@
+import Script from "next/script";
+
+export function GoogleAnalytics({ GA_MEASUREMENT_ID }) {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
